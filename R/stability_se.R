55d5994dd86bcce11a1682d53d0513393f0bfaf0
Sciurus365 <jingmeng.cui@outlook.com>
2024-07-09 21:52:02 +0800

    24.7.9.3
    trial with boot::boot

--- R/stability_se.R
+++ R/stability_se.R
@@ -1,28 +1,36 @@
 
 #' Calculate the standard error and related range estimates for the stability metrics
 #'
-#' @param bootnet A `bootnet` object calculated by [bootnet::bootnet()], with the default `type = "IsingFit"`. See [bootnet::bootnet()] for details.
+#' @param data
 #' @inheritParams calculate_stability.2d_Isingland
+#' @inheritParams boot::boot
+#' @param ... Parameters passed to [boot::boot()]
 #'
 #' @export
-calculate_stability_se <- function(bootnet, split_value = 0.5 * bootnet$boots[[1]]$nNode, ...) {
-	# check if the bootnet object is calculated using IsingFit
-
-	if (b1$boots[[1]]$default != "IsingFit") {
-		rlang::abort("The bootnet object is not calculated using IsingFit. Please use `default = 'IsingFit'` in bootnet::bootnet().")
-	}
-
-	boots <- bootnet$boots
-	all_s <- lapply(boots, function(b){
-		l <- make_2d_Isingland(thresholds = b$intercepts, weiadj = b$graph)
-		s <- calculate_stability.2d_Isingland(l, split_value = split_value, ...)
+calculate_stability_se <- function(data, split_value = 0.5 * ncol(data), R = 1000, ...) {
+	boot::boot(data = data, statistic = function(x, indicies, split_value, ...) {
+		n <- IsingFit::IsingFit(x = x, progressbar = FALSE, ...)
+		l <- make_2d_Isingland(thresholds = n$thresholds, weiadj = n$weiadj)
+		s <- calculate_stability.2d_Isingland(l, split_value = split_value)
 		c(s$stability1, s$stability2, s$stability_diff)
-	})
+	}, R = R, split_value = split_value, ...)
 
-	all_s <- do.call(rbind, all_s)
-	colnames(all_s) <- c("stability1", "stability2", "stability_diff")
 
-	return(all_s)
+	# if (b1$boots[[1]]$default != "IsingFit") {
+	# 	rlang::abort("The bootnet object is not calculated using IsingFit. Please use `default = 'IsingFit'` in bootnet::bootnet().")
+	# }
+	#
+	# boots <- bootnet$boots
+	# all_s <- lapply(boots, function(b){
+	# 	l <- make_2d_Isingland(thresholds = b$intercepts, weiadj = b$graph)
+	# 	s <- calculate_stability.2d_Isingland(l, split_value = split_value, ...)
+	# 	c("stability1" = s$stability1, "stability2" = s$stability2, "stability1_diff" = s$stability_diff)
+	# })
+	#
+	# all_s <- do.call(rbind, all_s)
+	# colnames(all_s) <- c("stability1", "stability2", "stability_diff")
+	#
+	# return(all_s)
 }
 
 
