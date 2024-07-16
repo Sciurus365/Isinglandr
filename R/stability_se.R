614b3c6022934dc50b453e5ba676f7e42e1a67ed
Sciurus365 <jingmeng.cui@outlook.com>
2024-07-17 00:49:19 +0800

    24.7.17.1
    a working version with se, ci, and p value

--- R/stability_se.R
+++ R/stability_se.R
@@ -1,36 +1,40 @@
-
 #' Calculate the standard error and related range estimates for the stability metrics
 #'
-#' @param data
+#' @param data A matrix of binary data
 #' @inheritParams calculate_stability.2d_Isingland
 #' @inheritParams boot::boot
+#' @param IsingFit_options Parameters passed to [IsingFit::IsingFit()]. Temporarily not effective.
+#' @param boot.ci_options Parameters passed to [boot::boot.ci()]
+#' @param boot.pval_options Parameters passed to [boot.pval::boot.pval()]
 #' @param ... Parameters passed to [boot::boot()]
 #'
 #' @export
-calculate_stability_se <- function(data, split_value = 0.5 * ncol(data), R = 1000, ...) {
-	boot::boot(data = data, statistic = function(x, indicies, split_value, ...) {
-		n <- IsingFit::IsingFit(x = x, progressbar = FALSE, ...)
+calculate_stability_se <- function(data, split_value = 0.5 * ncol(data), R = 1000, IsingFit_options = list(), boot.ci_options = list(type = "perc"), boot.pval_options = list(), ...) {
+
+	if (ncol(data) >= 20) {
+		rlang::warn("The number of variables is large. It may take a long time for bootstrapping.")
+	}
+
+	boot_res <- boot::boot(data = data, statistic = function(x, indices, split_value) {
+		n <- do.call(IsingFit::IsingFit, append(list(), list(x = x[indices,], progressbar = FALSE), after = 0))
 		l <- make_2d_Isingland(thresholds = n$thresholds, weiadj = n$weiadj)
 		s <- calculate_stability.2d_Isingland(l, split_value = split_value)
-		c(s$stability1, s$stability2, s$stability_diff)
+		c("sability1" = s$stability1, "sability2" = s$stability2, "sability_diff" = s$stability_diff)
 	}, R = R, split_value = split_value, ...)
 
+	output <- broom::tidy(boot_res)
 
-	# if (b1$boots[[1]]$default != "IsingFit") {
-	# 	rlang::abort("The bootnet object is not calculated using IsingFit. Please use `default = 'IsingFit'` in bootnet::bootnet().")
-	# }
-	#
-	# boots <- bootnet$boots
-	# all_s <- lapply(boots, function(b){
-	# 	l <- make_2d_Isingland(thresholds = b$intercepts, weiadj = b$graph)
-	# 	s <- calculate_stability.2d_Isingland(l, split_value = split_value, ...)
-	# 	c("stability1" = s$stability1, "stability2" = s$stability2, "stability1_diff" = s$stability_diff)
-	# })
-	#
-	# all_s <- do.call(rbind, all_s)
-	# colnames(all_s) <- c("stability1", "stability2", "stability_diff")
-	#
-	# return(all_s)
+	output <- output %>%
+		dplyr::mutate(row.index = dplyr::row_number()) %>%
+		dplyr::rowwise() %>%
+		dplyr::mutate(
+			p.value = do.call(boot.pval::boot.pval, append(boot.pval_options, list(boot_res = boot_res, index = row.index), after = 0)),
+			conf.low = do.call(boot::boot.ci, append(boot.ci_options, list(boot.out = boot_res, index = row.index), after = 0))$percent[4],
+			conf.high = do.call(boot::boot.ci, append(boot.ci_options, list(boot.out = boot_res, index = row.index), after = 0))$percent[5]
+		) %>%
+		dplyr::mutate(p.value = ifelse(statistic == 0 & std.error == 0, NA, p.value)) %>%
+		dplyr::ungroup() %>%
+		dplyr::select(-row.index)
+
+	return(output)
 }
-
-
--- R/utils.R
+++ R/utils.R
@@ -52,4 +52,4 @@ abort_bad_argument <- function(arg, must, not = NULL) {
 }
 
 # Undefined global
-utils::globalVariables(c(".", ".data", ".env", "barrier", "density", "dist", "freq", "landscape", "n_active", "sim", "sim_output", "sum_freq", "sum_state", "thresholds_list", "time", "v", "weiadj_list", "MDDConnectivity", "MDDThresholds", "stability", "stability_measures", "sum_state_x", "sum_state_y"))
+utils::globalVariables(c(".", ".data", ".env", "barrier", "density", "dist", "freq", "landscape", "n_active", "sim", "sim_output", "sum_freq", "sum_state", "thresholds_list", "time", "v", "weiadj_list", "MDDConnectivity", "MDDThresholds", "stability", "stability_measures", "sum_state_x", "sum_state_y", "p.value", "row.index", "statistic", "std.error"))
