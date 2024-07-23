1e1e34d76622a324daa1743d710b62069c07cabd
Sciurus365 <jingmeng.cui@outlook.com>
2024-07-24 01:31:23 +0800

    24.7.24.1
    move to bca

--- R/stability_se.R
+++ R/stability_se.R
@@ -1,16 +1,20 @@
-#' Calculate the standard error and related range estimates for the stability metrics
+#' Calculate the standard error, confidence interval, and p-value for the stability difference between two phases of an Ising network
+#'
+#' Currently only the stability difference is calculated. The stability difference is the difference between the stability of the first phase and the stability of the second phase. The stability of a single phase is often highly zero-inflated and leads to difficulty in calculating the standard error. The stability difference is more regularly distributed and is more meaningful to perform the range estimate.
 #'
 #' @param data A matrix of binary data
 #' @inheritParams calculate_stability.2d_Isingland
 #' @inheritParams boot::boot
 #' @param IsingFit_options Parameters passed to [IsingFit::IsingFit()]
 #' @param Isingland_options Parameters passed to [make_2d_Isingland()]
-#' @param boot.ci_options Parameters passed to [boot::boot.ci()]. `type = "basic"` is set as the default because the stability metric tends to have zero-inflated, skewed distribution, and the basic method is the most robust to such distribution. The same reason is applied to the p-value calculation.
-#' @param boot.pval_options Parameters passed to [boot.pval::boot.pval()]
+#' @param boot.ci_options Parameters passed to [boot::boot.ci()]. `type = "bca"` is set as the default because it performs best in most cases. See references for more information.
+#' @param boot.pval_options Parameters passed to [boot.pval::boot.pval()]. `type = "bca"` is set as the default because it performs best in most cases. See references for more information.
 #' @param ... Parameters passed to [boot::boot()]
 #'
 #' @export
-calculate_stability_se <- function(data, split_value = 0.5 * ncol(data), R = 1000, IsingFit_options = list(), Isingland_options = list(), boot.ci_options = list(type = "basic"), boot.pval_options = list(type = "basic"), ...) {
+#' @references Puth, M.-T., Neuhäuser, M., & Ruxton, G. D. (2015). On the variety of methods for calculating confidence intervals by bootstrapping. Journal of Animal Ecology, 84(4), 892–897. https://doi.org/10.1111/1365-2656.12382
+#' @seealso [compare_stability()] for comparing the stability differences of two groups.
+calculate_stability_se <- function(data, split_value = 0.5 * ncol(data), R = 1000, IsingFit_options = list(plot = FALSE), Isingland_options = list(), boot.ci_options = list(type = "bca"), boot.pval_options = list(type = "bca"), ...) {
   if (ncol(data) >= 20) {
     cli::cli_warn("The number of variables is large. It may take a long time for bootstrapping.")
   }
@@ -19,22 +23,31 @@ calculate_stability_se <- function(data, split_value = 0.5 * ncol(data), R = 100
     n <- do.call(IsingFit::IsingFit, append(.IsingFit_options, list(x = x[indices, ], progressbar = FALSE), after = 0))
     l <- do.call(make_2d_Isingland, append(.Isingland_options, list(thresholds = n$thresholds, weiadj = n$weiadj), after = 0))
     s <- calculate_stability.2d_Isingland(l, split_value = split_value)
-    c("stability1" = s$stability1, "stability2" = s$stability2, "stability_diff" = s$stability_diff)
+    # c("stability1" = s$stability1, "stability2" = s$stability2, "stability_diff" = s$stability_diff)
+    c("stability_diff" = s$stability_diff)
   }, R = R, split_value = split_value, .IsingFit_options = IsingFit_options, .Isingland_options = Isingland_options, ...)
 
   output <- broom::tidy(boot_res)
 
-  output <- output %>%
-    dplyr::mutate(row.index = dplyr::row_number()) %>%
-    dplyr::rowwise() %>%
-    dplyr::mutate(
-      p.value = do.call(boot.pval::boot.pval, append(boot.pval_options, list(boot_res = boot_res, index = row.index), after = 0)),
-      conf.low = do.call(boot::boot.ci, append(boot.ci_options, list(boot.out = boot_res, index = row.index), after = 0))[[boot.ci_options$type]][4],
-      conf.high = do.call(boot::boot.ci, append(boot.ci_options, list(boot.out = boot_res, index = row.index), after = 0))[[boot.ci_options$type]][5]
-    ) %>%
-    dplyr::mutate(p.value = ifelse(statistic == 0 & std.error == 0, NA, p.value)) %>%
-    dplyr::ungroup() %>%
-    dplyr::select(-row.index)
+  ## in the following code, if you got this error, "estimated adjustment 'a' is NA", that probably means you should increase the number of bootstrap samples (`R`).
+
+  tryCatch({
+    output <- output %>%
+      dplyr::mutate(row.index = dplyr::row_number()) %>%
+      dplyr::rowwise() %>%
+      dplyr::mutate(
+        p.value = do.call(boot.pval::boot.pval, append(boot.pval_options, list(boot_res = boot_res, index = row.index), after = 0)),
+        conf.low = do.call(boot::boot.ci, append(boot.ci_options, list(boot.out = boot_res, index = row.index), after = 0))[[boot.ci_options$type]][4],
+        conf.high = do.call(boot::boot.ci, append(boot.ci_options, list(boot.out = boot_res, index = row.index), after = 0))[[boot.ci_options$type]][5]
+      ) %>%
+      dplyr::ungroup() %>%
+      dplyr::select(-row.index)
+  }, error = function(e) {
+  	original_message <- conditionMessage(e)
+    cli::cli_abort(c("An error occurred. The original error message is as follows:",
+    								 x = original_message,
+    								 i = "If you got this error, 'estimated adjustment 'a' is NA', that probably means you should increase the number of bootstrap samples (`R`)."))
+  })
 
   return(
     structure(
@@ -55,40 +68,71 @@ print.stability_se <- function(x, ...) {
 }
 
 
-#' Compare the stability metrics between two Ising landscapes
+#' Compare the stability difference between two groups
 #'
-#' @param stability_se_1,stability_se_2 Two objects of class \code{stability_se}
 #' @inheritParams calculate_stability_se
-#'
+#' @param data1,data2 Two matrices of binary data
 #' @export
-compare_stability <- function(stability_se_1, stability_se_2, boot.ci_options = list(type = "basic"), boot.pval_options = list(type = "basic")) {
-  new_boot <- stability_se_1$boot_res
-  new_boot$t0 <- stability_se_1$boot_res$t0 - stability_se_2$boot_res$t0
-  new_boot$t <- stability_se_1$boot_res$t - stability_se_2$boot_res$t
-  new_boot$data <- NULL
-  new_boot$seed <- NULL
-  new_boot$strata <- NULL
-  new_boot$weights <- NULL
+#' @seealso [calculate_stability_se()] for getting a range estimation of the stability difference of a single group.
+#' @inherit calculate_stability_se references
+compare_stability <- function(data1, data2, split_value = 0.5 * ncol(data), R = 1000, IsingFit_options = list(plot = FALSE), Isingland_options = list(), boot.ci_options = list(type = "bca"), boot.pval_options = list(type = "bca"), ...) {
+	if (ncol(data1) >= 20) {
+		cli::cli_warn("The number of variables is large. It may take a long time for bootstrapping.")
+	}
 
-  output <- broom::tidy(new_boot)
-  boot_res <- new_boot
+	if (ncol(data1) != ncol(data2)) {
+		cli::cli_abort("The number of variables in data1 and data2 must be the same.")
+	}
 
-  output <- output %>%
-    dplyr::mutate(row.index = dplyr::row_number()) %>%
-    dplyr::rowwise() %>%
-    dplyr::mutate(
-      p.value = do.call(boot.pval::boot.pval, append(boot.pval_options, list(boot_res = boot_res, index = row.index), after = 0)),
-      conf.low = do.call(boot::boot.ci, append(boot.ci_options, list(boot.out = boot_res, index = row.index), after = 0))[[boot.ci_options$type]][4],
-      conf.high = do.call(boot::boot.ci, append(boot.ci_options, list(boot.out = boot_res, index = row.index), after = 0))[[boot.ci_options$type]][5]
-    ) %>%
-    dplyr::mutate(p.value = ifelse(statistic == 0 & std.error == 0, NA, p.value)) %>%
-    dplyr::ungroup() %>%
-    dplyr::select(-row.index)
+	data1 <- cbind(data1, group = 1)
+	data2 <- cbind(data2, group = 2)
 
-  return(
-    structure(
-      list(stats = output, boot_res = new_boot),
-      class = c("compare_stability_se", "stability_se", "list")
-    )
-  )
+	data <- rbind(data1, data2)
+
+	boot_res <- boot::boot(data = data, statistic = function(x, indices, split_value, .IsingFit_options, .Isingland_options) {
+		x <- x[indices,]
+		x1 <- x[x[,"group"] == 1, -ncol(x)]
+		x2 <- x[x[,"group"] == 2, -ncol(x)]
+
+		n1 <- do.call(IsingFit::IsingFit, append(.IsingFit_options, list(x = x1, progressbar = FALSE), after = 0))
+		n2 <- do.call(IsingFit::IsingFit, append(.IsingFit_options, list(x = x2, progressbar = FALSE), after = 0))
+		l1 <- do.call(make_2d_Isingland, append(.Isingland_options, list(thresholds = n1$thresholds, weiadj = n1$weiadj), after = 0))
+		l2 <- do.call(make_2d_Isingland, append(.Isingland_options, list(thresholds = n2$thresholds, weiadj = n2$weiadj), after = 0))
+		s1 <- calculate_stability.2d_Isingland(l1, split_value = split_value)
+		s2 <- calculate_stability.2d_Isingland(l2, split_value = split_value)
+
+		# c("group1_stability1" = s1$stability1, "group1_stability2" = s1$stability2,
+		# 	"group1_stability_diff" = s1$stability_diff, "group2_stability1" = s2$stability1,
+		# 	"group2_stability2" = s2$stability2, "group2_stability_diff" = s2$stability_diff,
+		# 	"diff_stability_diff" = s1$stability_diff - s2$stability_diff)
+		c("group1_stability_diff" = s1$stability_diff, "group2_stability_diff" = s2$stability_diff, "diff_stability_diff" = s1$stability_diff - s2$stability_diff)
+	}, R = R, split_value = split_value, .IsingFit_options = IsingFit_options, .Isingland_options = Isingland_options, ...)
+
+	output <- broom::tidy(boot_res)
+
+	tryCatch(
+	{
+		output <- output %>%
+			dplyr::mutate(row.index = dplyr::row_number()) %>%
+			dplyr::rowwise() %>%
+			dplyr::mutate(
+				p.value = do.call(boot.pval::boot.pval, append(boot.pval_options, list(boot_res = boot_res, index = row.index), after = 0)),
+				conf.low = do.call(boot::boot.ci, append(boot.ci_options, list(boot.out = boot_res, index = row.index), after = 0))[[boot.ci_options$type]][4],
+				conf.high = do.call(boot::boot.ci, append(boot.ci_options, list(boot.out = boot_res, index = row.index), after = 0))[[boot.ci_options$type]][5]
+			) %>%
+			dplyr::ungroup() %>%
+			dplyr::select(-row.index)
+	}, error = function(e) {
+		original_message <- conditionMessage(e)
+		cli::cli_abort(c("An error occurred. The original error message is as follows:",
+										 x = original_message,
+										 i = "If you got this error, 'estimated adjustment 'a' is NA', that probably means you should increase the number of bootstrap samples (`R`)."))
+	})
+
+	return(
+		structure(
+			list(stats = output, boot_res = boot_res),
+			class = c("stability_se", "list")
+		)
+	)
 }
